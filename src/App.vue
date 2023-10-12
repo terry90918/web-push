218a4db3c9a09b0f8e5c27b61dd73928f837d09b
terry90918 <zxtw17985321@gmail.com>
2023-10-12 13:00:06 +0800

    feat: web push

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App v1.0.0"/>
+    <HelloWorld msg="Welcome to Your Vue.js App v1.0.1"/>
   </div>
 </template>
 
