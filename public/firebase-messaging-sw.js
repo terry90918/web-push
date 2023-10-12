d087648ce68d389c70ccd71ee48ca4ba5149cb18
terry90918 <zxtw17985321@gmail.com>
2023-10-12 15:06:41 +0800

    feat: web push

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,7 +18,7 @@ const messaging = firebase.messaging()
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload)
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = '[firebase-messaging-sw.js] Received background message ' + payload.notification.title
   const notificationOptions = {
     body: payload.notification.body,
     icon: payload.notification.image,
